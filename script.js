73c3e998541a507b932807ffedc01745fe75d24f
Domenico Fantauzzo <d.fantauzzo@geolog.it>
2025-12-13 15:53:36 +0100

    inded 3.3.26

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* =========================================================
    [1] RIFERIMENTI DOM
    ========================================================= */
-const APP_VERSION = "3.3.25";
+const APP_VERSION = "3.3.26";
 
 const audio = document.getElementById("audioPlayer");
 const listContainer = document.getElementById("trackList");
