3dfcb1d20c6c04137c4e1c8a7c27cc86379ac4d3
Domenico Fantauzzo <d.fantauzzo@geolog.it>
2025-12-15 22:24:12 +0100

    version 3.3.32

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* =========================================================
    [1] RIFERIMENTI DOM
    ========================================================= */
-const APP_VERSION = "3.3.31";
+const APP_VERSION = "3.3.32";
 
 const audio = document.getElementById("audioPlayer");
 const listContainer = document.getElementById("trackList");
